e34ba4c9175f5aa81265746fa5ee8711c7c1118c
Timothy Eich <94193381+TimothyEich@users.noreply.github.com>
2023-07-20 17:00:36 -0500

    Hero section completed

---
+++ my-nextjs-portfolio/src/app/Mentor.tsx
@@ -0,0 +1,65 @@
+import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
+import page from "/Users/Timot/Desktop/Portfolio/my-nextjs-portfolio/src/app/page"
+
+
+export default function Mentor() {
+  return (
+<div className="bg-red-600">
+<nav className="bg-transparent h-24">
+  <div className="max-w-[25] mx-auto px-2 sm:px-6 lg:px-8">
+    <div className="flex justify-between">
+      <div className="">
+          <Link href="#">
+          <img className="max-w-[30%] h-auto justify-self-start mt-[4%]" src="/TE-Logo.png" alt="Logo"/> 
+          </Link>
+      </div>
+      <div className="hidden md:block ml-auto">
+        <div className="pt-[15%] ">
+          <a href="mentor" className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg uppercase font-bold ">Mentorship</a>
+          <a href="contact" className="text-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg uppercase font-bold ">Say Hello!</a>
+        </div>
+      </div>
+      <AiOutlineMenu size={40} className="md:hidden mt-[5%] mr-[3%]"/>
+    </div>
+  </div>
+</nav>
+
+
+  <main className="flex flex-col items-center justify-between p-24 mt-[6%]">
+    <div className="mb-32 text-center bg-transparent">
+      <h1 className="text-5xl font-bold mb-6">Welcome to My Portfolio!</h1>
+      
+      
+      <p className="text-xl text-gray-600 bg-slate-50 rounded-lg shadow-md p-6">
+        Blah blah blah. Here is a small intro about myself to gather your attention.
+      </p>
+    </div>
+  </main>
+    <div className="grid grid-cols-2 gap-8">
+      <div className="bg-slate-50 rounded-lg shadow-md p-6">
+        <h2 className="text-3xl font-semibold mb-3">Boop</h2>
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales pharetra felis, sit amet malesuada augue luctus et.
+        </p>
+      </div>
+      <div className="bg-slate-50 rounded-lg shadow-md p-6">
+        <h2 className="text-3xl font-semibold mb-3">Beep</h2>
+        <p className="text-gray-600">
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.
+        </p>
+      </div>
+    </div>
+
+    <div className="mt-24">
+      <img
+        src="/image.jpg"
+        alt="Image"
+        className="w-full h-auto rounded-lg shadow-lg"
+      />
+    </div>
+  
+</div>
+  );
+}
+
--- my-nextjs-portfolio/src/app/page.tsx
+++ my-nextjs-portfolio/src/app/page.tsx
@@ -1,113 +1,86 @@
-import Image from 'next/image'
+//import Image from 'next/image'
+import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
+import Mentor from "./Mentor";
+import Typewriter from 'typewriter-effect';
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Thank you for visiting my portfolio, my name is&nbsp;
-          <code className="font-mono font-bold">Tim Eichenlaub</code>
+<div className="bg-slate-100">
+<nav className="bg-slate-100 h-1">
+  <div className="max-w-[100%] mx-auto sm:px-6 lg:px-8">
+    <div className="flex justify-between ">
+      <div className="">
+        <Link href="/">
+          <img className="max-w-[30%] h-auto justify-self-start mt-[4%] hover:opacity-[30] hover:max-w-[35%]" src="/TE-Logo.png" alt="Logo"/> 
+        </Link>
+      </div>
+      <div className="hidden md:flex space-x-2 mr-[2%] mt-[3%] "> 
+        <Link href="/Mentor">
+          <div className="button-container inline-block">
+            <button className=" text-indigo-700 hover:bg-slate-800 hover:text-slate-100 px-5 py-3 text-lg rounded-md uppercase font-bold ">Mentoring</button>
+          </div>
+        </Link>
+        <Link href="/Contact">
+          <div className="button-container inline-block flex-shrink-0">
+            <button className="text-indigo-700 hover:bg-slate-800 hover:text-slate-100 px-3 py-2 rounded-md text-lg uppercase font-bold border-4 border-indigo-700 hover:border-slate-800">Say Hello!</button>
+          </div>
+        </Link>
+      </div>
+      <AiOutlineMenu size={40} className="md:hidden mt-[5%] mr-[3%]"/>
+    </div>
+  </div>
+</nav>
+
+
+
+
+  <main className="bg-slate-100 flex flex-col items-center justify-between p-24 mt-[15%]">
+    <div className="mb-32 text-center flex flex-col items-center bg-transparent justify-center">
+      <h1 className="text-5xl font-bold mb-8 text-slate-800 ">Welcome to my page!</h1>
+      
+      <p className="text-xl text-slate-800 p-10 max-w-[65%] mb-[6%]  rounded-md shadow-inner shadow-indigo-800">
+        I'm an Army Veteran changing careers and looking to land a spot as a Web Developer. I enjoy making pages look amazing and finding ways to incorporate newly learned skills into my projects. 
+      </p>
+    </div>
+  </main>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+  <div className="bg-indigo-700">&nbsp;</div>
+
+
+
+  <div className=" bg-slate-100 flex flex-col items-center pt-[5%] pb-[5%]">
+    <div className="grid grid-cols-2 gap-8 mt-[15%] bg-slate-100 pb-[10%]">
+      <div className="bg-slate-50 rounded-lg p-6 ml-[5%] shadow-lg shadow-indigo-700 mt-[-70%] mb-[20%]">
+        <h2 className="text-3xl font-semibold mb-3">Boop</h2>
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales pharetra felis, sit amet malesuada augue luctus et.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-slate-50 rounded-lg p-6 mr-[5%] shadow-lg shadow-indigo-700 mt-[-70%] mb-[20%]">
+        <h2 className="text-3xl font-semibold mb-3">Beep</h2>
+        <p className="text-gray-600">
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+    <p className=" text-lg text-center content-center">Big ass paragraph! Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sit amet venenatis odio. </p>
+  </div>
+</div>
+  );
 }
+
